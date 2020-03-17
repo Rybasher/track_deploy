96b6c6ab9504e421719aad240742309d584cd7f5
Rybasher <kirary99@gmail.com>
2020-03-17 04:31:30 +0200

    some new styles

--- delivery/views.py
+++ delivery/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-
+from cart.cart import Cart
 # Create your views here.
 
 
 def delivery(request):
+    cart = Cart(request)
     path = request.path_info
     url_split = path.split('/')
     print (url_split)
     url = url_split[2:]
     str_url = "/".join(url)
-    return render(request, 'delivery/delivery.html', {'path': str_url})
+    return render(request, 'delivery/delivery.html', {'path': str_url, 'catt': cart})
