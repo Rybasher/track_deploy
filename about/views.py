e135c31530498d2f411d1a6637c295c8e26ab846
Rybasher <kirary99@gmail.com>
2020-03-18 02:45:32 +0200

    new

--- about/views.py
+++ about/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-
+from cart.cart import Cart
 # Create your views here.
 from django.conf.global_settings import LANGUAGE_CODE
 
 
 def about(request):
+    cart = Cart(request)
     path = request.path_info
     url_split = path.split('/')
     url = url_split[2:]
     str_url = "/".join(url)
 
-    return render(request, 'about/about.html', {'path': str_url})
+    return render(request, 'about/about.html', {'path': str_url, 'cart': cart})
